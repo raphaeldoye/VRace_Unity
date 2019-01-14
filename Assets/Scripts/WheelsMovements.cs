f17962b86b34f140229a8197e2f521b8ebdfd997
Raphael Doye <raphaeldoye@gmail.com>
2019-01-13 21:21:57 -0500

    ajout d'un script

---
+++ Assets/Scripts/WheelsMovements.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelsMovements : MonoBehaviour
+{
+	[SerializeField] private Transform[] frontWheels;
+	[SerializeField] private Transform[] backWheels;
+	[SerializeField] private int maxSpeed = 50;
+
+	public void SpinWheels(int speedFraction) // -1 a 1
+	{
+		for (int i = 0; i < frontWheels.Length; i++)
+		{
+			//frontWheels[i].Rotate()
+		}
+	}
+
+	public void TurnWheels(int intensity) // -1 a 1
+	{
+
+	}
+}
