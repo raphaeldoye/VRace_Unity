d2de09b5404b115ba89aa133b7d3953354f772ae
Raphael Doye <raphaeldoye@gmail.com>
2019-03-05 11:23:53 -0500

    ajout et optimisation des algo de reduction de bruit

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Linq;
 
 
 public class GhostController : MonoBehaviour
 {
+	public enum SporadicFilter { None, AutomateTreshold, ManualTreshold};
+	public enum SelectedValueFilter { LastValue, Mean, Mode, Median}
+
 	VehicleTransforms vTransforms;
 	VehicleTransform vTransform;
 	string path = "Assets/Resources/vehicleTransforms.txt";
 	private bool locked = true;
 	public Vector3 decalage = Vector3.zero;
+	[Header("developper settings")]
+	[Space(10)]
+	public int readDataPerFrame = 1;
+
+	public SporadicFilter filter_SporadicNoise = SporadicFilter.None;
+	public SelectedValueFilter filter_selectedValue = SelectedValueFilter.LastValue;
+	public float manualTreshold = 1.0f;
+	public float sporadicSigmaMultiplier = 2.0f;
+	public float averageAlpha = 0.5f;
+
+	private Vector3 previousPosition;
+	private Vector3 previousRotation;
 
 	void Start()
     {
@@ -22,17 +38,178 @@ public class GhostController : MonoBehaviour
 	{
 		if (!locked)
 		{
+			SetCarPosition();
+		}
+	}
+
+	private void SetCarPosition()
+	{
+		previousPosition = transform.position;
+		previousRotation = transform.eulerAngles;
+
+		List<Vector3> positions = new List<Vector3>();
+		List<Vector3> rotations = new List<Vector3>();
+		Vector3 position = new Vector3();
+		Vector3 rotation = new Vector3();
+		if (PopTransforms(positions, rotations, readDataPerFrame))
+		{
+
+			if (filter_SporadicNoise != SporadicFilter.None)
+			{
+				rotations.Add(Average(rotations));
+
+				positions.Insert(0, previousPosition);
+				rotations.Insert(0, previousRotation);
+
+				RemoveSporadicNoise(positions);
+				RemoveSporadicNoise(rotations);
+
+				positions.RemoveAt(0);
+				rotations.RemoveAt(0);
+
+				rotations.RemoveAt(rotations.Count - 1);
+			}
+
+				switch (filter_selectedValue)
+				{
+					case SelectedValueFilter.LastValue:
+						position = positions[positions.Count - 1];
+						rotation = rotations[rotations.Count - 1];
+						break;
+					case SelectedValueFilter.Mean:
+						position = Average(positions);
+						rotation = Average(rotations);
+					break;
+					case SelectedValueFilter.Mode:
+						position = Mode(positions);
+						rotation = Mode(rotations);
+					break;
+					case SelectedValueFilter.Median:
+					position = Median(positions);
+					rotation = Median(rotations);
+					break;
+					default:
+						break;
+				}
+				position = Average(positions);
+				rotation = Average(rotations);
+		
+
+			transform.position = position + decalage;
+			transform.eulerAngles = rotation;
+		}
+		else
+		{
+			gameObject.SetActive(false);
+		}
+
+	}
+
+	private bool PopTransforms(List<Vector3> pos, List<Vector3> rot, int iteration)
+	{
+		for (int i = 0; i < iteration; i++)
+		{
 			if (!vTransforms.Empty())
 			{
 				vTransform = vTransforms.Dequeue();
-				transform.position = vTransform.position + decalage;
-				transform.eulerAngles = vTransform.rotation;
+				pos.Add(vTransform.position);
+				rot.Add(vTransform.rotation);
 			}
 			else
 			{
-				gameObject.SetActive(false);
+				return false;
 			}
 		}
+
+		return true;
+	}
+
+	private void RemoveSporadicNoise(List<Vector3> positions)
+	{
+		float treshold = filter_SporadicNoise == SporadicFilter.AutomateTreshold ? GetTreshold(positions): manualTreshold;
+		int i = 1;
+		while (i <= positions.Count-2)
+		{
+			if (Distance(positions[i],positions[i-1]) > treshold && (Distance(positions[i], positions[i + 1]) > treshold))
+			{
+				positions.RemoveAt(i);
+			}
+			else
+			{
+				i++;
+			}
+		}
+	}
+
+	private Vector3 Mode (List<Vector3> positions)
+	{
+		//Vector3 modeVector = new Vector3();
+		var groups = positions.GroupBy(v => v);
+		int maxCount = groups.Max(g => g.Count());
+		Vector3 mode = groups.First(g => g.Count() == maxCount).Key;
+
+		return mode;
+	}
+
+	private Vector3 Median(List<Vector3> positions)
+	{
+		var orderedPositions = positions.OrderBy(p => p.magnitude);
+		Vector3 median = orderedPositions.ElementAt(positions.Count / 2) + orderedPositions.ElementAt((positions.Count - 1) / 2);
+		median /= 2;
+		return median;
+	}
+
+	private Vector3 Average (List<Vector3> positions)
+	{
+		//averageAlpha*
+		Vector3 average = Vector3.zero;
+		for (int i = 0; i < positions.Count; i++)
+		{
+			average += positions[i];
+		}
+
+		return average / positions.Count;
+	}
+
+	private float Distance(Vector3 pointA, Vector3 pointB)
+	{
+		return Vector3.Distance(pointA, pointB);
+	}
+
+	private float Distance(List<Vector3> positions)
+	{
+		float totaldistance = 0.0f;
+
+		for (int i = 0; i < positions.Count-2; i++)
+		{
+			totaldistance += Distance(positions[i], positions[i + 1]);
+		}
+
+		return totaldistance;
+	}
+
+	private float SquareSum (List<Vector3> positions, float averageDistance)
+	{
+		float squareSum = 0.0f;
+
+		for (int i = 0; i < positions.Count-2; i++)
+		{
+			float d = Distance(positions[i], positions[i + 1]);
+			float difference = d - averageDistance;
+			squareSum += difference * difference;
+		}
+
+		return squareSum;
+	}
+
+	private float GetTreshold(List<Vector3> positions)
+	{
+		float totalDistance = Distance(positions); // distance totale entre les points
+		float averageDistance = totalDistance / (positions.Count - 1); // distance moyenne
+		float squareSum = SquareSum(positions, averageDistance); // obtenir la somme des carré
+		float sigma = Mathf.Sqrt(squareSum / (positions.Count - 2)); // écart type
+
+		return averageDistance + (sporadicSigmaMultiplier * sigma);
 	}
 
 	public void UnlockMovement()
--- Assets/Scripts/MovementRecorder.cs
+++ Assets/Scripts/MovementRecorder.cs
@@ -7,16 +7,21 @@ public class MovementRecorder : MonoBehaviour
 {
 	VehicleTransforms transforms;
 	bool savedOnce = false;
-	string path = "Assets/Resources/vehicleTransforms.txt";
+	public string path = "Assets/Resources/vehicleTransforms.txt";
 	bool record = false;
 	public Transform trackerTransform;
+	[Header("Developper settings")]
+	public bool addNoise = false;
+	public int savedDataPerFrame = 1;
+	static System.Random rnd;
 
 
 	void Start()
     {
 		transforms = new VehicleTransforms();
 		Directory.CreateDirectory(Path.GetDirectoryName(path));
-    }
+		rnd = new System.Random();
+	}
 
 	private void Update()
 	{
@@ -31,19 +36,79 @@ public class MovementRecorder : MonoBehaviour
 	{
 		if (!savedOnce && record)
 		{
-			AddCarPosition();
+			if (addNoise)
+			{
+				AddCarPositionWithNoise(trackerTransform.position, trackerTransform.eulerAngles);
+			}
+			else
+			{
+				AddCarPosition(trackerTransform.position, trackerTransform.eulerAngles);
+			}
 		}
 	}
 
-	void AddCarPosition()
-	{
-
+	void AddCarPosition(Vector3 pos, Vector3 rot)
+	{	
 		VehicleTransform v = new VehicleTransform
 		{
-			position = trackerTransform.position,
-			rotation = transform.eulerAngles
+			position = pos,
+			rotation = rot
 		};
 		transforms.Push(v);
+	}
+
+	void AddCarPositionWithNoise(Vector3 pos, Vector3 rot)
+	{
+		for (int i = 0; i < savedDataPerFrame; i++)
+		{
+			if (rnd.Next(3) == 1)
+			{
+				if (rnd.Next(5) == 4) // gros bruit
+				{
+					AddCarPosition(pos, AddNoise(new Vector3(0, rot.y, 0), 20));
+				}
+				else // petit bruit
+				{
+					AddCarPosition(pos, AddNoise(new Vector3(0, rot.y, 0), 3));
+				}
+			}
+			else
+			{
+				AddCarPosition(pos,new Vector3(0, rot.y, 0));
+			}
+			
+		}
+	}
+
+Vector3 AddNoise(Vector3 value, int maxNoise)
+	{
+		float x = value.x;
+		float y = value.y;
+		float z = value.z;
+
+		if (x > 1)
+		{
+			if(rnd.Next(2) == 1)
+				x += (float)rnd.NextDouble() * maxNoise;
+			else
+				x -= (float)rnd.NextDouble() * maxNoise;
+		}
+		if (y > 1)
+		{
+			if (rnd.Next(2) == 1)
+				y += (float)rnd.NextDouble() * maxNoise;
+			else
+				y -= (float)rnd.NextDouble() * maxNoise;
+		}
+		if (z > 1)
+		{
+			if (rnd.Next(2) == 1)
+				z += (float)rnd.NextDouble() * maxNoise;
+			else
+				z -= (float)rnd.NextDouble() * maxNoise;
+		}
+
+		return new Vector3(x, y, z);
 	}
 
 	void SaveMovements()
